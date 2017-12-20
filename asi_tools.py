7c9450c74f767a059c18694b003c21237b3af6ad
alenkaComagic <alenka.tula2@gmail.com>
2017-12-20 18:01:39 +0300

    new aasi project files (alenka)

---
+++ asi_tools.py
@@ -0,0 +1,17 @@
+import sys, time
+import tools, start_uis_test, pageElements
+import loger 
+from loger import Loger as loger
+
+# создаем объект, для использования модуля описывающего элементы на страницах
+lk_elements  = pageElements.LK()
+
+class Asi_tools(tools.Uis_tools):
+	def __init__(self, driver):
+		self.driver = driver
+
+#ШФБЛОН(! Этой опции больше нет все методы перенести и перепроверить) консультант -- Общие настройки -- Шаблоны сообщений
+	@property
+	def general_settings_get_templates_list(self):
+	# (C) поиск записай на странице выбирает формирует список объектов из элементов в таблице шаблонов ответа, в списке только отображенные на странице элементы
+		return self.elements_list(object_type = 'table', search_type = 'contains', mask = 'id, \'commonsettings-page-tableview-\'')
---
+++ sanity_ppc.py
@@ -0,0 +1,22 @@
+import tools, pageElements, random, asi_tools, start_uis_test, sys
+from loger import Loger as loger
+from selenium.webdriver.support.ui import Select
+import time
+from time import gmtime, strftime
+from datetime import datetime
+from testrail import *
+
+
+lk_elements  = pageElements.LK()
+# импортируем настройки из предварительно подготовленного файла
+configLK = tools.User_config()
+configLK.set_lk_parametrs()
+
+loger.file_log(text = 'Start sanity (NEW mode) test', text_type = 'SUCCESS')
+
+# модуль инициализации
+main_unit = start_uis_test.Global_unit()
+driver = main_unit.init_browser(product_type = 'ff')
+
+tools = tools.Uis_tools(driver)
+asi = asi_tools.Asi_tools(driver)
